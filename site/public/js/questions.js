42850a6959411f3129a62b1cf6f7ced169a7796a
DIEGHOSANT0S <diego.santos@sptech.school>
2023-11-09 21:42:15 -0300

    adicionando um quiz feito com vetor

---
+++ site/public/js/questions.js
@@ -0,0 +1,50 @@
+export default  [
+  {
+    question: "Quem foi lançado na cova dos leões?",
+    answers: [
+      { option: "Daniel", correct: true },
+      { option: "Paulo", correct: false },
+      { option: "Davi", correct: false },
+    ],
+  },
+  {
+    question: "Quantos eram os discípulos de Jesus?",
+    answers: [
+      { option: "11", correct: false },
+      { option: "12", correct: true },
+      { option: "13", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o primeiro livro da Bíblia?",
+    answers: [
+      { option: "Mateus", correct: false },
+      { option: "Números", correct: false },
+      { option: "Gênesis", correct: true },
+    ],
+  },
+  {
+    question: "Qual a cidade em que Jesus nasceu?",
+    answers: [
+      { option: "Nazaré", correct: false },
+      { option: "Jerusalém", correct: false },
+      { option: "Belém", correct: true },
+    ],
+  },
+  {
+    question: "Quais os nomes dos autores dos Evangelhos na Bíblia?",
+    answers: [
+      { option: "Marcos, Timóteu, Paulo e João", correct: false },
+      { option: "Mateus, Marcos, Lucas e João", correct: true },
+      { option: "Mateus, Pedro, Lucas, João e Tiago", correct: false },
+    ],
+  },
+  {
+    question: "Qual o nome do último livro da Bíblia?",
+    answers: [
+      { option: "Atos", correct: false },
+      { option: "Apocalipse", correct: true },
+      { option: "Deuteronômio", correct: false },
+    ],
+  },
+];
